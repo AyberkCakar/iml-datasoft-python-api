3304806c6b788e30d04e0cccf194f081f28d10ab
AyberkCakar <ayberkcakar35@gmail.com>
2023-12-10 20:10:34 +0300

    feat: keras and tensorflow algorithms activated

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,16 +1,22 @@
 import pandas as pd
 from algorithms.abod import abod_outlier_detection
+from algorithms.autoencoder import autoencoder_outlier_detection
 from algorithms.baggingOutlier import feature_bagging_outlier_detection
 from algorithms.cblof import cblof_outlier_detection
+from algorithms.deepAutoencoders import deep_autoencoder_outlier_detection
 from algorithms.ensemleMethods import ensemble_outlier_detection
 from algorithms.gaussianMixture import gmm_outlier_detection
+from algorithms.gru import gru_anomaly_detection
 from algorithms.hasuraRequest import fetch_algorithm, fetch_real_dataset, fetch_simulator_dataset, fetch_algorithm_setting
 from algorithms.hbos import hbos_outlier_detection
 from algorithms.isolationForest import isolation_forest
 from algorithms.kmeans import kmeans_outlier_detection_with_metrics
 from algorithms.knn import knn_outlier_detection
 from algorithms.localOutlierFactor import lof_outlier_detection
+from algorithms.lstm import lstm_anomaly_detection
+from algorithms.lstmAutoencoder import lstm_autoencoder
 from algorithms.pca import pca_outlier_detection
+from algorithms.rnn import rnn_anomaly_detection
 from algorithms.robustCovariance import robust_covariance_outlier_detection_with_metrics
 from algorithms.rrcf import robust_random_cut_forest
 from algorithms.xgboost import xgboost_outlier_detection
@@ -21,24 +27,24 @@ failure_types = []
 def call_algorithm(algorithm_name, algorithm_settings_id, algorithmId, fetched_data, sensor_types):
     algorithm_functions = {
         'Isolation Forest': isolation_forest,
-        # 'LSTM Autoencoder': lstm_autoencoder,
+        'LSTM Autoencoder': lstm_autoencoder,
         'Local Outlier Factor': lof_outlier_detection,
-        'KNN': knn_outlier_detection,
-        # 'Autoencoder Outlier Detection': autoencoder_outlier_detection,
+        'KNN Outlier Detection': knn_outlier_detection,
+        'Autoencoder Outlier Detection': autoencoder_outlier_detection,
         'ABOD Outlier Detection': abod_outlier_detection,
         'Feature Bagging Outlier Detection': feature_bagging_outlier_detection,
         'Ensemble Outlier Detection': ensemble_outlier_detection,
-        'K-Means': kmeans_outlier_detection_with_metrics,
+        'K-Means Outlier Detection': kmeans_outlier_detection_with_metrics,
         'Robust Covariance Outlier Detection': robust_covariance_outlier_detection_with_metrics,
         'PCA Outlier Detection': pca_outlier_detection,
         'GMM Outlier Detection': gmm_outlier_detection,
         'HOBS Outlier Detection': hbos_outlier_detection,
-        # 'Deep Autoencoder Outlier Detection': deep_autoencoder_outlier_detection,
+        'Deep Autoencoder Outlier Detection': deep_autoencoder_outlier_detection,
         'CBLOF Outlier Detection': cblof_outlier_detection,
         'XGBoost Outlier Detection': xgboost_outlier_detection,
-        # 'LSTM Anomaly Detection': lstm_anomaly_detection,
-        # 'RNN Anomaly Detection': rnn_anomaly_detection,
-        # 'GRU Anomaly Detection': gru_anomaly_detection,
+        'LSTM Anomaly Detection': lstm_anomaly_detection,
+        'RNN Anomaly Detection': rnn_anomaly_detection,
+        'GRU Anomaly Detection': gru_anomaly_detection,
         'Robust Random Cut Forest': robust_random_cut_forest,
         # 'dbscan_outlier_detection': dbscan_outlier_detection,
         # 'oneclass_svm_outlier_detection': oneclass_svm_outlier_detection,
